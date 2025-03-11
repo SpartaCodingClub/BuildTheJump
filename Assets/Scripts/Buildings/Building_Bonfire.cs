1b463d7172b0d92175fb159821678cec9a46320f
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-03-11 16:18:57 +0900

    Feat: 모닥불 기능 추가

--- Assets/Scripts/Buildings/Building_Bonfire.cs
+++ Assets/Scripts/Buildings/Building_Bonfire.cs
@@ -1,4 +1,40 @@
+using UnityEngine;
+
 public class Building_Bonfire : BuildingObject
 {
+    private static readonly float INTERVAL = 2.0f;
 
+    private bool interaction;
+    private float timer;
+
+    private void Update()
+    {
+        if (interaction == false)
+        {
+            return;
+        }
+
+        timer += Time.deltaTime;
+        if (timer > INTERVAL)
+        {
+            timer -= INTERVAL;
+
+            Managers.Game.SetHP(10);
+            Managers.Game.SetSP(10);
+        }
+    }
+
+    public override void InteractionEnter(int damage = 0)
+    {
+        base.InteractionEnter(damage);
+        interaction = true;
+    }
+
+    public override void InteractionExit(bool isPlayer)
+    {
+        base.InteractionExit(isPlayer);
+
+        interaction = false;
+        timer = 0.0f;
+    }
 }
